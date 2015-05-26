4b5d1cfe17bfaf09be9da15adcbf10c1f9aa67a5
sariabueitah <sari.abueitah@gmail.com>
2015-05-26 22:02:48 +0300

    Create User using Devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :makes
 
   resources :models
@@ -17,6 +18,7 @@ Rails.application.routes.draw do
 
   get 'admin', to: 'admin#index'
 
+  root to: "cars#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150522100927) do
+ActiveRecord::Schema.define(version: 20150526185320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20150522100927) do
     t.integer  "report"
     t.integer  "car_make"
     t.integer  "car_model"
+    t.integer  "user_id"
   end
 
   create_table "colors", force: :cascade do |t|
@@ -102,5 +103,23 @@ ActiveRecord::Schema.define(version: 20150522100927) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
 end
