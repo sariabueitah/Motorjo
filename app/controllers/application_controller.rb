30759a7af81cc6f2672ec87f1eaed7617fe5f9c7
sariabueitah <sari.abueitah@gmail.com>
2015-10-17 23:10:14 +0300

    adding meta tags google analytics and alexa id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :authenticate_admin?
-  before_action :set_locale
+  before_action :set_locale, :setmeta
+  def setmeta
+    set_meta_tags description: 'أول موقع أردني متخصص في بيع وشراء السيارات والدراجات والاليات بالأردن الجديدة والمستعملة و هو موقعك المناسب للاعلانات المبوبة في الاردن'
+    @customKeywords = 'سيارات بالأردن, سيارات للبيع بالأردن ,سيارات مستعملة بالأردن ,سيارات جديدة بالأردن ,وكلاء سيارات بالأردن ,معاراض سيارات بالأردن ,Car In Jordan,Car For Sale In Jordan ,Car Showrooms In Jordan'
+  end
   def set_locale
     I18n.locale =  session[:locale] || http_accept_language.compatible_language_from(I18n.available_locales) || I18n.default_locale
     session[:locale] = I18n.locale
