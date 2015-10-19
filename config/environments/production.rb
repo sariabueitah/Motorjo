ee21475a99426f9bc795b9fd54fb12cdb6d9860e
sariabueitah <sari.abueitah@gmail.com>
2015-10-19 23:27:42 +0300

    hot fix mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.assets.raise_runtime_errors = true
+  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 end
