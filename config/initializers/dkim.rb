48274b732453c1d96f3cec798a31d600299f7973
sariabueitah <sari.abueitah@gmail.com>
2015-12-05 23:24:58 +0200

    dkim fix

--- config/initializers/dkim.rb
+++ config/initializers/dkim.rb
@@ -1,8 +1,8 @@
 if Rails.env.production?
   # Configure dkim globally (see above)
-  Dkim::domain      = 'YOURHOSTNAME.com'
-  Dkim::selector    = 'x'
-  Dkim::private_key = File.read('config/dkim/dkim.private.key')
+  Dkim::domain      = 'motorsjo.com'
+  Dkim::selector    = 'mail'
+  Dkim::private_key = File.read('/etc/opendkim/keys/motorsjo.com/mail.private')
   # This will sign all ActionMailer deliveries
   ActionMailer::Base.register_interceptor(Dkim::Interceptor)
 end
