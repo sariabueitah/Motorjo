288c2b8e851f927c0094d93c76a350602f4858fb
sariabueitah <sari.abueitah@gmail.com>
2015-12-05 21:08:15 +0200

    fixs

---
+++ app/models/devise_helper.rb
@@ -0,0 +1,24 @@
+module DeviseHelper
+  def devise_error_messages!
+    return "" if resource.errors.empty?
+
+    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+    byebug
+    sentence = I18n.t("errors.messages.not_saved",
+                      :count => resource.errors.count,
+                      :resource => resource.class.model_name.human.downcase)
+
+    html = <<-HTML
+    <div data-alert class="alert-box warning">
+      <ul>#{messages}</ul>
+    </div>
+    HTML
+
+    html.html_safe
+  end
+
+  def devise_error_messages?
+    resource.errors.empty? ? false : true
+  end
+
+end
