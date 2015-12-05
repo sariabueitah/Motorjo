fe58ed1f98fb8d0256f61f188d9f01562a2fc448
sariabueitah <sari.abueitah@gmail.com>
2015-12-06 00:21:56 +0200

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,10 +78,5 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.assets.raise_runtime_errors = true
   config.action_mailer.default_url_options = { :host => 'motorsjo.com' }
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:                 "motorsjo.com",
-    port:                    25,
-    enable_starttls_auto:    false
-  }
+  config.action_mailer.delivery_method = :sendmail
 end
