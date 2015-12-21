86e6cc862ae4574eb04169eb2ac257bf32ce059d
sariabueitah <sari.abueitah@gmail.com>
2015-12-21 20:14:38 +0200

    gmail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,14 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
   config.assets.raise_runtime_errors = true
   config.action_mailer.default_url_options = { :host => 'motorsjo.com' }
-  config.action_mailer.delivery_method = :sendmail
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address             => "smtp.gmail.com",
+    :port                => 587,
+    :domain              => "motorsjo.com",
+    :authentication      => "plain",
+    :user_name           => "motorsjo@motorsjo.com",
+    :password            => ENV['MOTORSJO_MAIL_PASSWORD'],
+    :enable_starttls_auto => true
+  }
 end
