b05aaf5aa1b0e0edeb2f11095a51c9561616cb98
sariabueitah <sari.abueitah@gmail.com>
2015-12-21 19:20:39 +0200

    bugfix

--- app/models/member.rb
+++ app/models/member.rb
@@ -2,4 +2,19 @@ class Member < ActiveRecord::Base
 	has_one :user, as: :meta, dependent: :destroy
 	validates :first_name, :last_name, :phone_number, :address, presence: true
   	accepts_nested_attributes_for :user
+  	City = {
+		"1" => "Amman",
+		"2" => "Zarqa",
+		"3" => "Irbid",
+		"4" => "Aqaba",
+		"5" => "as-Salt",
+		"6" => "Ar Ramtha",
+		"7" => "Madaba",
+		"8" => "Mafraq",
+		"9" => "Jerash",
+		"10" => "Ma'an",
+		"11" => "Tafilah",
+		"12" => "Karak",
+		"13" => "Ajloun"
+	}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  	has_many :cars
+  	has_many :cars, dependent: :destroy 
 	devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable, :confirmable
 	belongs_to :meta, polymorphic: true 
 end
